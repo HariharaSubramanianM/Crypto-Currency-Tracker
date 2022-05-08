1a70b2f338582130b2f4b1221369370bb8ba7e06
Harihara Subramanian M <74015897+HariharaSubramanianM@users.noreply.github.com>
2022-05-08 19:19:48 +0530

    Layout done
    HTML Layout prepared

--- cryptoapp/views.py
+++ cryptoapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import requests
+from django.conf import settings
 
 # Create your views here.
+
+def index(request):
+    apidata = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    return render(request,'index.html',{'apidata':apidata})
