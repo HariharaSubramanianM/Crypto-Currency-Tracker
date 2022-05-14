fe9006e343fadc83a7bb2bf3141554d215a20049
Harihara Subramanian M <74015897+HariharaSubramanianM@users.noreply.github.com>
2022-05-14 19:04:38 +0530

    Added Watchlist Feature
    Included a feature to track selected cryptocurrencies by adding them to a new table viewable below the original.

--- cryptoapp/views.py
+++ cryptoapp/views.py
@@ -6,4 +6,7 @@ from django.conf import settings
 
 def index(request):
     apidata = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false').json()
-    return render(request,'index.html',{'apidata':apidata})
+    return render(request,'cryptoapp/index.html',{'apidata':apidata})
+
+def watchlist(request):
+    return render(request,'cryptoapp/watchlist.html')
