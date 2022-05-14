bac4c1ad2bc90f0c6039e10f59cedf2bf1d5fd85
Harihara Subramanian M <74015897+HariharaSubramanianM@users.noreply.github.com>
2022-05-14 12:24:23 +0530

    API Included
    API from coingecko added

--- cryptoapp/views.py
+++ cryptoapp/views.py
@@ -5,5 +5,5 @@ from django.conf import settings
 # Create your views here.
 
 def index(request):
-    apidata = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    apidata = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false').json()
     return render(request,'index.html',{'apidata':apidata})
