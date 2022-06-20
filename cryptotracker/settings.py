d70996e3c944e21c9f7693279f41c9c25756c899
Harihara Subramanian M <74015897+HariharaSubramanianM@users.noreply.github.com>
2022-06-20 19:18:31 +0530

    Host Commit - 2

--- cryptotracker/settings.py
+++ cryptotracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u1@1qh151(4bt+6v2!r=3tj)n-l9(^!9%fd3d3r%b4(k9i!eao
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','cryptotrackeronline.herokuapp.com']
 
 
 # Application definition
